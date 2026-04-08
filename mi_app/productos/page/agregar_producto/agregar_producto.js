cbf993331977f32d17df32792d6a28a5e64cf95a
Jonas Silva <jonassilva580@gmail.com>
2026-04-07 21:07:38 -0300

    Se pueden editar productos, limpiar listas e imprimir por individual

--- mi_app/productos/page/agregar_producto/agregar_producto.js
+++ mi_app/productos/page/agregar_producto/agregar_producto.js
@@ -7,6 +7,7 @@ frappe.pages['agregar-producto'].on_page_load = function(wrapper) {
     });
 
     window.productosCreados = [];
+    window.tabIndex = 10;
 
     $(wrapper).html(`
         <div class="form-layout">
@@ -14,34 +15,35 @@ frappe.pages['agregar-producto'].on_page_load = function(wrapper) {
 
             <div class="form-group">
                 <label>Nombre *</label>
-                <input id="nombre" class="form-control" placeholder="Nombre del producto">
+                <input id="nombre" class="form-control" placeholder="Nombre del producto" tabindex="1">
             </div>
 
             <div class="form-group">
                 <label>Descripción</label>
-                <textarea id="descripcion" class="form-control" placeholder="Descripción"></textarea>
+                <textarea id="descripcion" class="form-control" placeholder="Descripción" tabindex="2"></textarea>
+            </div>
             </div>
 
             <div class="form-row">
                 <div class="col">
                     <label>Marca</label>
                     <div class="input-group">
-                        <select id="marca" class="form-control">
+                        <select id="marca" class="form-control" tabindex="3">
                             <option value="">Seleccionar...</option>
                         </select>
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-default" onclick="agregarNuevaMarca()" title="Agregar nueva marca">+</button>
+                            <button type="button" class="btn btn-default" onclick="agregarNuevaMarca()" title="Agregar nueva marca" tabindex="-1">+</button>
                         </span>
                     </div>
                 </div>
                 <div class="col">
                     <label>Categoría *</label>
                     <div class="input-group">
-                        <select id="categoria" class="form-control">
+                        <select id="categoria" class="form-control" tabindex="4">
                             <option value="">Seleccionar...</option>
                         </select>
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-default" onclick="agregarNuevaCategoria()" title="Agregar nueva categoría">+</button>
+                            <button type="button" class="btn btn-default" onclick="agregarNuevaCategoria()" title="Agregar nueva categoría" tabindex="-1">+</button>
                         </span>
                     </div>
                 </div>
@@ -51,30 +53,30 @@ frappe.pages['agregar-producto'].on_page_load = function(wrapper) {
                 <div class="col">
                     <label>Subcategoría</label>
                     <div class="input-group">
-                        <select id="subcategoria" class="form-control">
+                        <select id="subcategoria" class="form-control" tabindex="5">
                             <option value="">Seleccionar...</option>
                         </select>
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-default" onclick="agregarNuevaSubcategoria()" title="Agregar nueva subcategoría">+</button>
+                            <button type="button" class="btn btn-default" onclick="agregarNuevaSubcategoria()" title="Agregar nueva subcategoría" tabindex="-1">+</button>
                         </span>
                     </div>
                 </div>
                 <div class="col">
                     <label>Precio</label>
-                    <input id="precio" class="form-control" type="number" step="0.01" placeholder="Precio">
+                    <input id="precio" class="form-control" type="number" step="0.01" placeholder="Precio" tabindex="6">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="col">
                     <label>Warehouse</label>
-                    <select id="warehouse" class="form-control"></select>
+                    <select id="warehouse" class="form-control" tabindex="7"></select>
                 </div>
             </div>
 
             <div class="form-group">
                 <label>Talles (separados por coma) *</label>
-                <input id="talles" class="form-control" placeholder="S, M, L, XL">
+                <input id="talles" class="form-control" placeholder="S, M, L, XL" tabindex="8">
             </div>
 
             <div id="talles-container" class="form-group" style="display:none;">
@@ -82,10 +84,10 @@ frappe.pages['agregar-producto'].on_page_load = function(wrapper) {
                 <div id="cantidades-talles"></div>
             </div>
 
-            <button onclick="actualizarTalles()" class="btn btn-default">Agregar Talles</button>
-            <button onclick="crearProducto()" class="btn btn-primary">Guardar</button>
-            <button onclick="limpiarFormulario()" class="btn btn-default">Limpiar</button>
-            <button onclick="imprimirTodasEtiquetas()" class="btn btn-info" style="display:none;" id="btn-imprimir-todas">Imprimir Todas las Etiquetas</button>
+            <button onclick="actualizarTalles()" class="btn btn-default" tabindex="9">Agregar Talles</button>
+            <button onclick="crearProducto()" class="btn btn-primary" tabindex="100">Guardar</button>
+            <button onclick="limpiarFormulario()" class="btn btn-default" tabindex="101">Limpiar</button>
+            <button onclick="imprimirTodasEtiquetas()" class="btn btn-info" style="display:none;" id="btn-imprimir-todas" tabindex="102">Imprimir Todas las Etiquetas</button>
         </div>
         
         <div class="form-layout" style="margin-top: 30px;">
@@ -203,6 +205,9 @@ function actualizarTalles() {
     container.empty();
     
     var hayTalles = false;
+    var tabStock = 10;
+    var tabBarcode = 20;
+    
     tallesInput.forEach(function(t) {
         var talle = t.trim();
         if (talle) {
@@ -213,13 +218,15 @@ function actualizarTalles() {
                         <strong>${talle}</strong>
                     </div>
                     <div class="col-md-4">
-                        <input type="number" class="form-control cantidad-talle" data-talle="${talle}" placeholder="Cantidad" min="0">
+                        <input type="number" class="form-control cantidad-talle" data-talle="${talle}" placeholder="Stock" min="0" tabindex="${tabStock}">
                     </div>
                     <div class="col-md-6">
-                        <input type="text" class="form-control barcode-talle" data-talle="${talle}" placeholder="Código de barras" value="">
+                        <input type="text" class="form-control barcode-talle" data-talle="${talle}" placeholder="Código de barras" value="" tabindex="${tabBarcode}">
                     </div>
                 </div>
             `);
+            tabStock++;
+            tabBarcode++;
         }
     });
     
@@ -285,6 +292,12 @@ function crearProducto() {
             
             let producto = {
                 nombre: $("#nombre").val(),
+                descripcion: $("#descripcion").val(),
+                marca: $("#marca").val(),
+                categoria: $("#categoria").val(),
+                subcategoria: $("#subcategoria").val(),
+                precio: $("#precio").val(),
+                warehouse: $("#warehouse").val(),
                 cantidades: cantidades,
                 barcodes: barcodes_variantes
             };
@@ -315,11 +328,19 @@ function actualizarListaProductos() {
     
     window.productosCreados.forEach(function(prod, index) {
         let talles = Object.keys(prod.cantidades).join(", ");
+        let totalStock = Object.values(prod.cantidades).reduce((a,b)=>a+b, 0);
         let html = `
-            <div class="form-row" style="margin-bottom: 5px; padding: 5px; border-bottom: 1px solid #eee;">
-                <div class="col-md-4"><strong>${prod.nombre}</strong></div>
-                <div class="col-md-4">Talles: ${talles}</div>
-                <div class="col-md-4">Cantidades: ${Object.values(prod.cantidades).reduce((a,b)=>a+b, 0)}</div>
+            <div class="form-row" style="margin-bottom: 5px; padding: 5px; border-bottom: 1px solid #eee; align-items: center;">
+                <div class="col-md-3"><strong>${prod.nombre}</strong></div>
+                <div class="col-md-3">Talles: ${talles}</div>
+                <div class="col-md-2">Stock: ${totalStock}</div>
+                <div class="col-md-2">
+                    <button class="btn btn-xs btn-default" onclick="editarProducto(${index})" title="Editar">✏️</button>
+                    <button class="btn btn-xs btn-danger" onclick="eliminarProducto(${index})" title="Eliminar">🗑️</button>
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-xs btn-info" onclick="imprimirProductoIndividual(${index})" title="Imprimir etiquetas">🖨️</button>
+                </div>
             </div>
         `;
         container.append(html);
@@ -328,6 +349,87 @@ function actualizarListaProductos() {
     if (window.productosCreados.length > 0) {
         $("#btn-imprimir-todas").show();
     }
+}
+
+function editarProducto(index) {
+    let prod = window.productosCreados[index];
+    
+    $("#nombre").val(prod.nombre);
+    $("#descripcion").val(prod.descripcion || "");
+    $("#marca").val(prod.marca || "");
+    $("#categoria").val(prod.categoria || "");
+    $("#subcategoria").val(prod.subcategoria || "");
+    $("#precio").val(prod.precio || "");
+    $("#warehouse").val(prod.warehouse || "");
+    $("#talles").val(Object.keys(prod.cantidades).join(", "));
+    
+    actualizarTalles();
+    
+    setTimeout(function() {
+        $(".cantidad-talle").each(function() {
+            let talle = $(this).data("talle");
+            if (prod.cantidades[talle] !== undefined) {
+                $(this).val(prod.cantidades[talle]);
+            }
+        });
+        
+        $(".barcode-talle").each(function() {
+            let talle = $(this).data("talle");
+            if (prod.barcodes[talle]) {
+                $(this).val(prod.barcodes[talle]);
+            }
+        });
+    }, 100);
+    
+    window.productosCreados.splice(index, 1);
+    actualizarListaProductos();
+    
+    frappe.msgprint("Editando producto: " + prod.nombre);
+}
+
+function eliminarProducto(index) {
+    let prod = window.productosCreados[index];
+    window.productosCreados.splice(index, 1);
+    actualizarListaProductos();
+    frappe.msgprint("Producto eliminado: " + prod.nombre);
+}
+
+function imprimirProductoIndividual(index) {
+    let prod = window.productosCreados[index];
+    let etiquetas = [];
+    
+    for (let [talle, qty] of Object.entries(prod.cantidades)) {
+        let barcode = prod.barcodes[talle] || "";
+        for (let i = 0; i < qty; i++) {
+            etiquetas.push({
+                nombre: prod.nombre,
+                talle: talle,
+                barcode: barcode
+            });
+        }
+    }
+    
+    if (etiquetas.length === 0) {
+        frappe.msgprint("No hay etiquetas para imprimir");
+        return;
+    }
+    
+    frappe.call({
+        method: "mi_app.productos.page.agregar_producto.agregar_producto.generar_etiquetas_html",
+        args: {
+            etiquetas: JSON.stringify(etiquetas)
+        },
+        callback: function(r) {
+            if (r.message) {
+                let printWindow = window.open('', '_blank');
+                printWindow.document.write(r.message);
+                printWindow.document.close();
+                setTimeout(function() {
+                    printWindow.print();
+                }, 500);
+            }
+        }
+    });
 }
 
 function imprimirTodasEtiquetas() {
